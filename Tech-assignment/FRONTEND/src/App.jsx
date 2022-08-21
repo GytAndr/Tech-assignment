7cfc895acecf355b2826fdad4be6f65fe9dcbcf4
GytAndr <andriejunas@gmail.com>
2022-08-21 15:58:20 +0300

    added responsivness

--- Tech-assignment/FRONTEND/src/App.jsx
+++ Tech-assignment/FRONTEND/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import SearchForm from './components/SearchForm';
 
 function App() {
 	return (
-		<div>
+		<Container>
 			<SearchForm />
-		</div>
+		</Container>
 	);
 }
 
--- Tech-assignment/FRONTEND/src/components/SearchForm.jsx
+++ Tech-assignment/FRONTEND/src/components/SearchForm.jsx
@@ -1,37 +1,43 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export default function SearchForm() {
 	return (
-		<Form>
-			<Form.Group className="mb-3">
-				<Form.Label htmlFor="companyInputField">Company symbol</Form.Label>
-				<InputGroup>
-					<InputGroup.Text>
-						<i className="bi bi-building"></i>
-					</InputGroup.Text>
-					<Form.Control
-						type="text"
-						id="companyInputField"
-						placeholder="Enter symbol e.g. AAPL"
-					/>
-				</InputGroup>
-			</Form.Group>
-
-			<div className="input-group mb-3 p-2">
-				<span className="input-group-text primary">Start date:</span>
-				<input className="form-control" type="date" />
-			</div>
-			<div className="input-group mb-3 p-2">
-				<span className="input-group-text">End date:</span>
-				<input className="form-control" type="date" />
-			</div>
-			<div className="m-3 d-flex justify-content-center">
-				<Button variant="secondary" type="submit">
+		<Row>
+			<Col sm>
+				<Form.Group className="mb-3">
+					<Form.Label htmlFor="companyInputField">Company symbol</Form.Label>
+					<InputGroup>
+						<InputGroup.Text>
+							<i className="bi bi-building"></i>
+						</InputGroup.Text>
+						<Form.Control
+							type="text"
+							id="companyInputField"
+							placeholder="Enter symbol e.g. AAPL"
+						/>
+					</InputGroup>
+				</Form.Group>
+			</Col>
+			<Col sm>
+				<Form.Group className="mb-3">
+					<Form.Label htmlFor="startDateInputField">Start date:</Form.Label>
+					<Form.Control id="startDateInputField" type="date" />
+				</Form.Group>
+			</Col>
+			<Col sm>
+				<Form.Group className="mb-3">
+					<Form.Label htmlFor="startDateInputField">Start date:</Form.Label>
+					<Form.Control id="startDateInputField" type="date" />
+				</Form.Group>
+			</Col>
+			<Col sm>
+				<Button variant="secondary" type="button">
 					Search
 				</Button>
-			</div>
-		</Form>
+			</Col>
+		</Row>
 	);
 }
