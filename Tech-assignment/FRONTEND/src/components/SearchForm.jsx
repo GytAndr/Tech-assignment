15e940fb0be39e8d1f0bfdcd67f1427cf5df90e4
GytAndr <andriejunas@gmail.com>
2022-08-21 21:15:53 +0300

    regex works

--- Tech-assignment/FRONTEND/src/components/SearchForm.jsx
+++ Tech-assignment/FRONTEND/src/components/SearchForm.jsx
@@ -3,27 +3,50 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
 import { useState } from 'react';
 
 export default function SearchForm() {
-	//temp state
-	const [validated, setValidated] = useState(false);
-	const handleSubmit = (event) => {
-		const form = event.currentTarget;
-		if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
+	//Onchange make RegEx validation,
+	//if regex fail
+	//set isInvalid=true && SetErrorMsg()
+	//if regex pass
+	//set isInvalid=false && setErrorMsg(null)
+	//pass value to state => setInputValue(e.target.value)
+	const onChangeValidate = (e) => {
+		const regEx = new RegExp(/^[A-Za-z\s]*$/);
+		if (!regEx.test(e.target.value)) {
+			setRegIsValid(true);
+			setErrorMsg('Please enter only letters including space');
+		} else {
+			setRegIsValid(false);
+			setErrorMsg('');
+			setInputValue(e.target.value);
 		}
-
-		setValidated(true);
 	};
-
+	// const [validated, setValidated] = useState(false);
+	// const handleSubmit = (event) => {
+	// 	const form = event.currentTarget;
+	// 	if (form.checkValidity() === false) {
+	// 		event.preventDefault();
+	// 		event.stopPropagation();
+	// 	}
+	// 	setValidated(true);
+	// };
+	const [inputValue, setInputValue] = useState();
+	const [errorMsg, setErrorMsg] = useState('');
+	const [regIsValid, setRegIsValid] = useState();
 	return (
 		<Row>
-			<Form noValidate validated={validated} onSubmit={handleSubmit}>
+			<Form>
 				<Col sm>
 					<Form.Group className="mb-3">
 						<Form.Label htmlFor="companyInputField">Company symbol</Form.Label>
+
+						{regIsValid ? (
+							<Alert variant="danger small p-1">{errorMsg}</Alert>
+						) : null}
+
 						<InputGroup hasValidation className="position-relative">
 							<InputGroup.Text>
 								<i className="bi bi-building"></i>
@@ -33,16 +56,10 @@ export default function SearchForm() {
 								id="companyInputField"
 								placeholder="Enter symbol e.g. AAPL"
 								required
+								isInvalid={regIsValid}
+								maxLength="35"
+								onChange={onChangeValidate}
 							/>
-							<Form.Control.Feedback
-								tooltip
-								type="invalid"
-								className="position-absolute"
-								style={{ top: '-35px' }}
-							>
-								Search input should have only letters and less then 35
-								characters
-							</Form.Control.Feedback>
 						</InputGroup>
 					</Form.Group>
 				</Col>
