f00d52647a984297bfd1d62483806dc31356e4ab
GytAndr <andriejunas@gmail.com>
2022-08-21 17:54:05 +0300

    validation error added

--- Tech-assignment/FRONTEND/src/components/SearchForm.jsx
+++ Tech-assignment/FRONTEND/src/components/SearchForm.jsx
@@ -3,42 +3,67 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
 
 export default function SearchForm() {
+	//temp state
+	const [validated, setValidated] = useState(false);
+	const handleSubmit = (event) => {
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+
+		setValidated(true);
+	};
+
 	return (
 		<Row>
-			<Col sm>
-				<Form.Group className="mb-3">
-					<Form.Label htmlFor="companyInputField">Company symbol</Form.Label>
-					<InputGroup>
-						<InputGroup.Text>
-							<i className="bi bi-building"></i>
-						</InputGroup.Text>
-						<Form.Control
-							type="text"
-							id="companyInputField"
-							placeholder="Enter symbol e.g. AAPL"
-						/>
-					</InputGroup>
-				</Form.Group>
-			</Col>
-			<Col sm>
-				<Form.Group className="mb-3">
-					<Form.Label htmlFor="startDateInputField">Start date:</Form.Label>
-					<Form.Control id="startDateInputField" type="date" />
-				</Form.Group>
-			</Col>
-			<Col sm>
-				<Form.Group className="mb-3">
-					<Form.Label htmlFor="endDateInputField">Start date:</Form.Label>
-					<Form.Control id="endDateInputField" type="date" />
-				</Form.Group>
-			</Col>
-			<Col sm className="d-flex justify-content-center align-items-end mb-3">
-				<Button variant="secondary" type="button">
-					Search
-				</Button>
-			</Col>
+			<Form noValidate validated={validated} onSubmit={handleSubmit}>
+				<Col sm>
+					<Form.Group className="mb-3">
+						<Form.Label htmlFor="companyInputField">Company symbol</Form.Label>
+						<InputGroup hasValidation className="position-relative">
+							<InputGroup.Text>
+								<i className="bi bi-building"></i>
+							</InputGroup.Text>
+							<Form.Control
+								type="text"
+								id="companyInputField"
+								placeholder="Enter symbol e.g. AAPL"
+								required
+							/>
+							<Form.Control.Feedback
+								tooltip
+								type="invalid"
+								className="position-absolute"
+								style={{ top: '-35px' }}
+							>
+								Search input should have only letters and less then 35
+								characters
+							</Form.Control.Feedback>
+						</InputGroup>
+					</Form.Group>
+				</Col>
+				<Col sm>
+					<Form.Group className="mb-3">
+						<Form.Label htmlFor="startDateInputField">Start date:</Form.Label>
+						<Form.Control id="startDateInputField" type="date" />
+					</Form.Group>
+				</Col>
+				<Col sm>
+					<Form.Group className="mb-3">
+						<Form.Label htmlFor="endDateInputField">Start date:</Form.Label>
+						<Form.Control id="endDateInputField" type="date" />
+					</Form.Group>
+				</Col>
+				<Col sm className="d-flex justify-content-center align-items-end mb-3">
+					<Button variant="secondary" type="submit">
+						Search
+					</Button>
+				</Col>
+			</Form>
 		</Row>
 	);
 }
